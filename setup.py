06b2d2e912c260c6cb539a2312a7c1d76ef0f3c2
elija feigl <elija-feigl@users.noreply.github.com>
2024-07-03 09:47:43 +0200

    fix (color): use random color if none specified.
    replaces raise ValueError with debug log

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nanodesign",
-    version="1.18",
+    version="1.18.1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=(
